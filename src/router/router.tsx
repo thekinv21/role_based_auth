3404a92db32f442b8cd5e75f3fca6cc144ba44bd
Vadim Kiniabaev <thekinv21@gmail.com>
2024-06-01 19:15:34 +0300

    FEAT:[practical][vadim]: added not found page router

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -5,9 +5,15 @@ import { PATH_URL } from '@/config/path.config'
 import { ProtectedRoute } from './ProtectedRoute'
 import { AdminPage } from '@/screens/admin/AdminPage'
 import { LoginPage } from '@/screens/auth/login'
+import { NotFoundPage } from '@/screens/error/404'
 import { HomePage } from '@/screens/home/HomePage'
 
 export const routes: RouteObject[] = [
+	{
+		path: '*',
+		element: <NotFoundPage />
+	},
+
 	{
 		path: PATH_URL.HOME,
 		element: (
---
+++ src/screens/error/404.tsx
@@ -0,0 +1,11 @@
+export function NotFoundPage() {
+	return (
+		<div className='flex h-screen w-full items-center justify-center'>
+			<div className='flex flex-col items-center justify-center'>
+				<h1 className='text-3xl font-bold text-white'>
+					404 - Page is not found
+				</h1>
+			</div>
+		</div>
+	)
+}
