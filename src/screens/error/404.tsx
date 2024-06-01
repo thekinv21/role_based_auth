4e267d76b290253c18890e21292cb0c8b67110af
Vadim Kiniabaev <thekinv21@gmail.com>
2024-06-01 23:14:53 +0300

    fıx:[practical][vadim]: connect login page with backend and added logout functionality and navbar for home page also created 404 page

--- src/screens/error/404.tsx
+++ src/screens/error/404.tsx
@@ -1,3 +1,5 @@
+import { PATH_URL } from '@/config/path.config'
+
 export function NotFoundPage() {
 	return (
 		<div className='flex h-screen w-full items-center justify-center'>
@@ -5,6 +7,16 @@ export function NotFoundPage() {
 				<h1 className='text-3xl font-bold text-white'>
 					404 - Page is not found
 				</h1>
+
+				<button
+					type='button'
+					onClick={() => {
+						window.location.href = PATH_URL.HOME
+					}}
+					className='my-10 rounded-md border-none bg-purple-600 px-10 py-1.5 outline-none transition-all duration-500 ease-in-out hover:bg-purple-700'
+				>
+					Go Home
+				</button>
 			</div>
 		</div>
 	)
